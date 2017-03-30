9eea93d291f8be66c26db1c5cc83a539ae285551
임종식 <whdtlr1218@gmail.com>
2017-03-30 20:44:32 +0900

    플래그

--- JongSik/angular2/project/src/app/jinhyeok/jinhyeok.component.ts
+++ JongSik/angular2/project/src/app/jinhyeok/jinhyeok.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-jinhyeok',
+  /*templateUrl 대신에 template: '' 를 이용하여
+  * ' '안에 html 소스를 넣어서 간단한 컴포넌트를 정의할 수 있다.
+  * template를 사용하면 html파일이 필요가 없다.
+  */
   templateUrl: './jinhyeok.component.html',
   styleUrls: ['./jinhyeok.component.css']
 })
 export class JinhyeokComponent implements OnInit {
   a : string = "text";
-  
+  name = 'Jinheok';
+
   constructor() { }
 
   ngOnInit() {
